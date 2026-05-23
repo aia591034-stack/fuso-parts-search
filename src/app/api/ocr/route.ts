388adff8bed7e4ede2d9fc6c7ffd7f8eeae118c1
syuttyoseibi <daishi2matsuda@gmail.com>
2026-05-24 01:19:38 +0900

    8

--- src/app/api/ocr/route.ts
+++ src/app/api/ocr/route.ts
@@ -12,7 +12,7 @@ export async function POST(request: Request) {
     }
 
     const model = genAI.getGenerativeModel({ 
-      model: 'gemini-1.5-flash'
+      model: 'gemini-1.5-flash-8b'
     })
 
     const arrayBuffer = await file.arrayBuffer()
